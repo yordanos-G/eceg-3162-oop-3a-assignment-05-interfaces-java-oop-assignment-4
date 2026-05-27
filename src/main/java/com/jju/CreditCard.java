d3466cb2b32749630a2075591e0a8bcecd38eeca
yordanos-G <yordilulu114@gmail.com>
2026-05-27 22:43:40 +0300

    Update CreditCard.java

--- src/main/java/com/jju/CreditCard.java
+++ src/main/java/com/jju/CreditCard.java
@@ -1,14 +1,17 @@
-package com.jju;
-
+// Credit card payment class
 public class CreditCard implements PaymentMethod {
+
+    // Store card number
     private String cardNumber;
 
+    // Constructor
     public CreditCard(String cardNumber) {
         this.cardNumber = cardNumber;
     }
 
+    // Process payment using credit card
     @Override
     public void processPayment(double amount) {
-        // TODO: Print "Charging $[amount] to Card: [cardNumber]"
+        System.out.println("Charging $" + amount + " to Card: " + cardNumber);
     }
 }
